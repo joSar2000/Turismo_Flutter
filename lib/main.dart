4973b2feed17aec9e71e54d79bca97f0df06d3d6
joSar2000 <jorgitosar@gmail.com>
2021-06-23 15:20:21 -0500

    Update del Main

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:turismo_flutter/pages/viewturista_table6_1.dart';
+import 'package:turismo_flutter/pages/viewturista_table7.dart';
 import 'package:turismo_flutter/pages/viewturista_table6.dart';
-import 'package:turismo_flutter/pages/viewturista_table6_2.dart';
+import 'package:turismo_flutter/pages/viewturista_table8.dart';
 import 'package:turismo_flutter/pages/viewturista_table6_2_1.dart';
 import 'package:turismo_flutter/pages/viewturista_table6_3.dart';
 
@@ -21,6 +21,7 @@ class MyApp extends StatelessWidget {
       initialRoute: turismTable6.ROUTE,
       routes: {
         turismTable6.ROUTE : (_) => turismTable6(),
+
         //turismTable6_1.ROUTE : (_) => turismTable6_1(),
         //turismTable6_2.ROUTE: (_) => turismTable6_2(),
         //turismTable6_2_1.ROUTE: (_) => turismTable6_2_1(),
