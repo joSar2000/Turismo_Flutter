6807e4dbb0260bd037dc5439ec71ff3a0106af33
cfjimbo <38107757+cfjimbo@users.noreply.github.com>
2021-07-15 12:04:40 -0500

    Carga de archivos tablas 1-5

---
+++ lib/src/Screens/datosGenerales.dart
@@ -0,0 +1,78 @@
+import 'package:app_flutter_turismo/src/Screens/ubicacion.dart';
+import 'package:flutter/material.dart';
+
+class DatosGenerales extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color(0xff885566),
+        title: Text("DATOS GENERALES", 
+        style: TextStyle(
+                    fontSize: 30.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white70,
+              )
+        ),
+        
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 10.0),
+        child: Container(
+          decoration: new BoxDecoration(color: Colors.white70),
+          child: Column(
+            
+            children: [
+              TextField(
+                decoration: InputDecoration(labelText: "Categoria"),
+                style: TextStyle(
+                    fontSize: 18.0,
+                    color: Colors.brown.shade800,
+                    fontWeight: FontWeight.bold,
+                    fontStyle: FontStyle.normal,
+                    decorationStyle: TextDecorationStyle.solid,
+              )
+              ),
+              TextField(
+                decoration: InputDecoration(labelText: "Tipo"),
+                style: TextStyle(
+                    fontSize: 18.0,
+                    color: Colors.brown.shade800,
+                    fontWeight: FontWeight.bold,
+                    fontStyle: FontStyle.normal,
+                    decorationStyle: TextDecorationStyle.solid,
+              )
+              ),
+              TextField(
+                decoration: InputDecoration(labelText: "Subtipo"),
+                style: TextStyle(
+                    fontSize: 18.0,
+                    color: Colors.brown.shade800,
+                    fontWeight: FontWeight.bold,
+                    fontStyle: FontStyle.normal,
+                    decorationStyle: TextDecorationStyle.solid,
+              )
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, 'ubicacion');
+                  },
+                  style: ElevatedButton.styleFrom(
+                  primary: Colors.brown,
+                  padding: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+                  textStyle: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold)),
+                  child: Text("Siguiente"),
+                  )
+            ],
+          ),
+        ),
+      ),
+      
+    );
+  }
+}
